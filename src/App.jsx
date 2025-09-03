7556c937a2ae2b49ac720fc1be8df8a0112d9b75
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-09-03 14:26:15 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PostForm from '../components/PostForm';
+//https://**67c5b4f3351c081993fb1ab6**.mockapi.io**/api**/posts
+function App() {
+
+  return (
+    <div className='container my-5'>
+      <h1>Add new post</h1>
+    </div >
+
+
+  )
+}
+
+export default App
